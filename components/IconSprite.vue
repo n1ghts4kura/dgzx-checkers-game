31b1575f95ee5c3aeba182823da5970276ba9dc5
n1ghts4kura <1825259836@qq.com>
2026-05-16 08:11:32 +0800

    feat: add hint lightbulb icon to IconSprite

--- components/IconSprite.vue
+++ components/IconSprite.vue
@@ -39,6 +39,10 @@
     <g v-else-if="name === 'refresh'">
       <path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z" fill="currentColor"/>
     </g>
+    <!-- hint (lightbulb) -->
+    <g v-else-if="name === 'hint'">
+      <path d="M9 21c0 .55.45 1 1 1h4c.55 0 1-.45 1-1v-1H9v1zm3-19C8.14 2 5 5.14 5 9c0 2.38 1.19 4.47 3 5.74V17c0 .55.45 1 1 1h6c.55 0 1-.45 1-1v-2.26c1.81-1.27 3-3.36 3-5.74 0-3.86-3.14-7-7-7zm2.85 11.1l-.85.6V16h-4v-2.3l-.85-.6C7.8 12.16 7 10.63 7 9c0-2.76 2.24-5 5-5s5 2.24 5 5c0 1.63-.8 3.16-2.15 4.1z" fill="currentColor"/>
+    </g>
   </svg>
 </template>
 
@@ -49,7 +53,7 @@ export default {
     name: {
       type: String,
       required: true,
-      validator: (v) => ['add_circle', 'grid_on', 'apps', 'menu_open', 'settings', 'arrow_back', 'undo', 'refresh'].includes(v)
+      validator: (v) => ['add_circle', 'grid_on', 'apps', 'menu_open', 'settings', 'arrow_back', 'undo', 'refresh', 'hint'].includes(v)
     },
     size: {
       type: [Number, String],
